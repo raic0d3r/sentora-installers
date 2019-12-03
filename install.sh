598c7dc88d5b9dd4911abbcb58a1d67633d7160c
raic0d3r <36988513+raic0d3r@users.noreply.github.com>
2019-12-03 04:31:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -133,7 +133,7 @@ elif [[ "$OS" = "Ubuntu" || "$OS" = "debian" ]]; then
     
     DB_PCKG="mysql-server"
     HTTP_PCKG="apache2"
-    PHP_PCKG="apache2-mod-php5.6"
+    PHP_PCKG="apache2-mod-php7.3"
     BIND_PCKG="bind9"
 fi
   
@@ -1043,13 +1043,13 @@ if [[ "$OS" = "CentOs" ]]; then
     PHP_INI_PATH="/etc/php.ini"
     PHP_EXT_PATH="/etc/php.d"
 elif [[ "$OS" = "Ubuntu" || "$OS" = "debian" ]]; then
-    $PACKAGE_INSTALLER ibapache2-mod-php5.6 libapache2-mod-php5.6-dbgsym libphp5.6-embed libphp5.6-embed-dbgsym php5.6 php5.6-bcmath php5.6-bcmath-dbgsym php5.6-bz2 php5.6-bz2-dbgsym php5.6-cgi php5.6-cgi-dbgsym php5.6-cli php5.6-cli-dbgsym php5.6-common php5.6-common-dbgsym php5.6-curl php5.6-curl-dbgsym php5.6-dba php5.6-dba-dbgsym php5.6-dev php5.6-enchant php5.6-enchant-dbgsym php5.6-fpm php5.6-fpm-dbgsym php5.6-gd php5.6-gd-dbgsym php5.6-gmp php5.6-gmp-dbgsym php5.6-imap php5.6-imap-dbgsym php5.6-interbase php5.6-interbase-dbgsym php5.6-intl php5.6-intl-dbgsym php5.6-json php5.6-json-dbgsym php5.6-ldap php5.6-ldap-dbgsym php5.6-mbstring php5.6-mbstring-dbgsym php5.6-mcrypt php5.6-mcrypt-dbgsym php5.6-mysql php5.6-mysql-dbgsym php5.6-odbc php5.6-odbc-dbgsym php5.6-opcache php5.6-opcache-dbgsym php5.6-pgsql php5.6-pgsql-dbgsym php5.6-phpdbg php5.6-phpdbg-dbgsym php5.6-pspell php5.6-pspell-dbgsym php5.6-readline php5.6-readline-dbgsym php5.6-recode php5.6-recode-dbgsym php5.6-snmp php5.6-snmp-dbgsym php5.6-soap php5.6-soap-dbgsym php5.6-sqlite3 php5.6-sqlite3-dbgsym php5.6-sybase php5.6-sybase-dbgsym php5.6-tidy php5.6-tidy-dbgsym php5.6-xml php5.6-xml-dbgsym php5.6-xmlrpc php5.6-xmlrpc-dbgsym php5.6-xsl php5.6-zip php5.6-zip-dbgsym
+    $PACKAGE_INSTALLER libapache2-mod-php7.3 libapache2-mod-php7.3-dbgsym libphp7.3-embed libphp7.3-embed-dbgsym php7.3-bcmath php7.3-bcmath-dbgsym php7.3-bz2 php7.3-bz2-dbgsym php7.3-cgi php7.3-cgi-dbgsym php7.3-cli php7.3-cli-dbgsym php7.3-common php7.3-common-dbgsym php7.3-curl php7.3-curl-dbgsym php7.3-dba php7.3-dba-dbgsym php7.3-dev php7.3-enchant php7.3-enchant-dbgsym php7.3-fpm php7.3-fpm-dbgsym php7.3-gd php7.3-gd-dbgsym php7.3-gmp php7.3-gmp-dbgsym php7.3-imap php7.3-imap-dbgsym php7.3-interbase php7.3-interbase-dbgsym php7.3-intl-dbgsym php7.3-json php7.3-json-dbgsym php7.3-ldap php7.3-ldap-dbgsym php7.3-mbstring php7.3-mbstring-dbgsym php7.3-mysql php7.3-mysql-dbgsym php7.3-odbc php7.3-odbc-dbgsym php7.3-opcache php7.3-opcache-dbgsym php7.3-pgsql php7.3-pgsql-dbgsym php7.3-phpdbg php7.3-phpdbg-dbgsym php7.3-pspell php7.3-pspell-dbgsym php7.3-readline php7.3-readline-dbgsym php7.3-recode php7.3-recode-dbgsym php7.3-snmp php7.3-snmp-dbgsym php7.3-soap php7.3-soap-dbgsym php7.3-sqlite3 php7.3-sqlite3-dbgsym php7.3-sybase php7.3-sybase-dbgsym php7.3-tidy php7.3-tidy-dbgsym php7.3-xml php7.3-xml-dbgsym php7.3-xmlrpc php7.3-xmlrpc-dbgsym php7.3-xsl php7.3-zip-dbgsym 
     if [ "$VER" = "14.04" ]; then
         php5enmod mcrypt  # missing in the package for Ubuntu 14, is this needed for debian 8 as well?
 #    else
 #        $PACKAGE_INSTALLER php5-suhosin-extension
     fi
-    PHP_INI_PATH="/etc/php/5.6/apache2/php.ini"
+    PHP_INI_PATH="/etc/php/7.3/apache2/php.ini"
 fi
 # Setup php upload dir
 mkdir -p $PANEL_DATA/temp
