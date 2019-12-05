2f206932aa2de8bce7b71ac8fdbdce0f57e9248a
raic0d3r <36988513+raic0d3r@users.noreply.github.com>
2019-12-05 03:07:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1043,7 +1043,7 @@ if [[ "$OS" = "CentOs" ]]; then
     PHP_INI_PATH="/etc/php.ini"
     PHP_EXT_PATH="/etc/php.d"
 elif [[ "$OS" = "Ubuntu" || "$OS" = "debian" ]]; then
-    $PACKAGE_INSTALLER libapache2-mod-php7.0 libphp7.0-embed php7.0-bz2 php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-intl php7.0-json php7.0-mbstring php7.0-mcrypt php7.0-mysql php7.0-opcache php7.0-pgsql php7.0-phpdbg php7.0-readline php7.0-xml php7.0-zip php7.0-bcmath php7.0-cgi php7.0-dba php7.0-dev php7.0-enchant php7.0-gmp php7.0-imap php7.0-interbase php7.0-ldap php7.0-odbc php7.0-pspell php7.0-recode php7.0-snmp php7.0-soap php7.0-sqlite3 php7.0-sybase php7.0-tidy php7.0-xmlrpc php7.0-xsl php7cc 
+    $PACKAGE_INSTALLER libapache2-mod-php libphp-embed php-bz2 php-cli php-common php-curl php-fpm php-pear php-gd php-intl php-json php-mbstring php-mcrypt php-mysql php-opcache php-pgsql php-phpdbg php-readline php-xml php-zip php-bcmath php-cgi php-dba php-dev php-enchant php-gmp php-imap php-interbase php-ldap php-odbc php-pspell php-recode php-snmp php-soap php-sqlite3 php-sybase php-tidy php-xmlrpc php-xsl 
     if [ "$VER" = "14.04" ]; then
         php5enmod mcrypt  # missing in the package for Ubuntu 14, is this needed for debian 8 as well?
 #    else
